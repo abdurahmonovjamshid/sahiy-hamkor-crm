413fd0c89120ddfe73fcc708ed6749d761bee2ae
abdurahmonovjamshid <abdurahmonovjamshid060@gmail.com>
2023-11-05 15:14:21 +0500

    add features to anvaraka seh

--- Anvaraka_sklad/views.py
+++ Anvaraka_sklad/views.py
@@ -26,11 +26,31 @@ def warehouse_add(sender, instance, created, **kwargs):
 def warehouse_presave(sender, instance, **kwargs):
     if instance.pk:
         old_warehouse = Warehouse.objects.get(pk=instance.pk)
+
         component = instance.component
         component.total = component.total - \
             (old_warehouse.quantity*old_warehouse.quantity_in_measurement) + \
             (instance.quantity*instance.quantity_in_measurement)
         component.save()
+
+        if old_warehouse.component.component.filter(quantity_in_measurement=old_warehouse.quantity_in_measurement):
+            product_component = old_warehouse.component.component.get(
+                quantity_in_measurement=old_warehouse.quantity_in_measurement)
+
+            product_component.quantity -= old_warehouse.quantity
+            if product_component.quantity < 0:
+                product_component.quantity = 0
+            product_component.save()
+
+        product_component, created = ProductComponent.objects.get_or_create(
+            product=instance.component, quantity_in_measurement=instance.quantity_in_measurement)
+
+        if not created:
+            product_component.quantity += instance.quantity
+            product_component.save()
+        else:
+            product_component.quantity = instance.quantity
+            product_component.save()
 
 
 @receiver(post_delete, sender=Warehouse)
@@ -38,6 +58,14 @@ def warehouse_delete(sender, instance, **kwargs):
     component = instance.component
     component.total -= (instance.quantity*instance.quantity_in_measurement)
     component.save()
+
+    if instance.component.component.filter(quantity_in_measurement=instance.quantity_in_measurement):
+        product_component = instance.component.component.get(
+            quantity_in_measurement=instance.quantity_in_measurement)
+        product_component.quantity -= instance.quantity
+        if product_component.quantity < 0:
+            product_component.quantity = 0
+        product_component.save()
 
 
 @receiver(post_save, sender=Sales)
