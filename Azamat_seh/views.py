69d09d024a110831cfe45a603cf2c9084cf7cfc4
abdurahmonovjamshid <abdurahmonovjamshid060@gmail.com>
2024-02-22 21:54:02 +0500

    change algarithm

--- Azamat_seh/views.py
+++ Azamat_seh/views.py
@@ -40,9 +40,6 @@ def subtract_component_total(sender, instance, created, **kwargs):
             component = product_component.component
             component.total -= total_quantity_by_component
 
-            if component.total < 0:
-                component.total = 0
-
             component.save()
 
 
